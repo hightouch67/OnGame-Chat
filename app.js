54feb2bedb4d09523fe0d8ce9b8e724f2cdb7820
hightouch <hightouch@hotmail.fr>
2018-12-01 23:05:40 +0100

    chatpat

--- app.js
+++ app.js
@@ -108,6 +108,10 @@ app.configure('development', function(){
 });
 
 app.get('/', function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
   if(!req.headers.cookie)
   {
     res.redirect('/signin');
