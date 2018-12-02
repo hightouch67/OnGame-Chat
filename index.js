8592792d6abad779f8ac2a2c79acf6eebd7059f5
hightouch <hightouch@hotmail.fr>
2018-12-02 02:07:32 +0100

    up

--- index.js
+++ index.js
@@ -50,9 +50,27 @@ var chatters = [];
 
 // Store messages in chatroom
 var chat_messages = [];
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // Express Middleware
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
